df0c28f687a2b96300a5c152749a68578badfece
meganm672 <meganm672@gmail.com>
2023-10-05 11:12:10 -0400

    styled the page with mui and grid

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -4,18 +4,22 @@ SingleBook component and view its details. */
 
 import React, {useState, useEffect } from "react"
 
+import { useNavigate } from "react-router-dom"
+
 import Typography from '@mui/material/Typography';
 import  Button  from "@mui/material/Button";
-import { Card, CardActionArea, CardContent, CardMedia } from "@mui/material";
+import { Card, CardActions, CardContent, CardMedia, Box, Grid, Container } from "@mui/material";
 
 export default function AllBooks(){
     const [books, setBooks]= useState([]);
     const [error, setError] = useState(null);
 
+    // const navigate = useNavigate();
+
     useEffect(() =>{
         async function fetchBookData(){
             try{
-                const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books")
+                const response = await fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books");
             const result = await response.json();
             console.log(result.books);
             setBooks(result.books)
@@ -28,35 +32,40 @@ export default function AllBooks(){
     },[])
 
     return(
-       <Card>
+       <Box>
             <Typography variant="h3">Books In The Libary</Typography>
             {error && !books && (<p> Failed to load books from api</p>)}
-            {books
-            ?(
-                books.map((book) =>{
-                    return(
-                        <Card key={book.title} >
-                        <CardMedia
-                            component="img"
-                            alt={book.title}
-                            height="400"
-                            // width={"300"}
-                            image={book.coverimage} 
-                        />
-                        <CardContent>
-                            <Typography variant="h3">{book.title}</Typography>
-                            <Typography><b> Author: </b>{book.author}</Typography>
-                            <Typography> <b>Description: </b>{book.description}</Typography>
-                            <Typography><b>Availible: </b> {book.available}</Typography>
-                        </CardContent>
-                            <CardActionArea>
-                                {/* <Button onClick={()=> navigate("/books/" + books.id)} >Book Info</Button>  */}
-                            </CardActionArea>
-                     
-                    </Card>
-                    )
-                })
-            ) : !error && <p>Loading...</p>}
-       </Card>
+         
+                <Grid container spacing={2}>
+                    {books
+                    ?(
+                    
+                        books.map((book) =>{
+                            return(
+                                <Grid item key={book.title}>
+                                    <Card sx={{maxWidth: 350, margin: 2}} >
+                                        <CardMedia
+                                            component="img"
+                                            alt={book.title}
+                                            height="500"
+                                            image={book.coverimage} 
+                                            />
+                                        <CardContent>
+                                            <Typography variant="h3">{book.title}</Typography>
+                                            <Typography><b> Author: </b>{book.author}</Typography>
+                                            <Typography> <b>Description: </b>{book.description}</Typography>
+                                            <Typography><b>Available: </b> {book.available ? "true" : "false"}</Typography>
+                                        </CardContent>
+                                        <CardActions>
+                                            <Button onClick={()=> navigate("/books/" + books.id)} >Book Info</Button> 
+                                        </CardActions>
+                                    </Card>
+                                 </Grid>
+                                )
+                                })
+                    ) : !error && <p>Loading...</p>}
+                </Grid>
+       
+       </Box>
     )
 }
