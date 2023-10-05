b2b8f7843e90f1d0ccbb71ab627e51d9f99adafb
meganm672 <meganm672@gmail.com>
2023-10-05 18:51:19 -0400

    updating correct

--- src/components/Books.jsx
+++ src/components/Books.jsx
@@ -70,25 +70,3 @@ export default function AllBooks(){
     )
 }
 
-import React, { useState, useEffect } from "react";
-function SingleBook() {
-  const [book, setSingleBook] = useState([]);
-  const [error, setError] = useState(null);
-  useEffect(() => {
-    fetch("https://fsa-book-buddy-b6e748d1380d.herokuapp.com/api/books", {
-      method: "GET",
-      
-    })
-      .then((SingleBook) => SingleBook.json())
-      .then((fetchBookData) => {
-        setSingleBook(result.SingleBook);
-        console.log(SingleBook);
-      })
-      .catch((error) => console.log(error));
-  }, []);
-  return (
-    <div>
-    
-    </div>
-  );
-}
