0dbb102c3897d08e942671e362d65a2d43913c77
Nicole Padilla <npadilla8@bloomberg.net>
2023-10-08 19:58:49 -0400

    Edited Account component to display the user details

--- src/components/Account.jsx
+++ src/components/Account.jsx
@@ -1,37 +1,42 @@
 /* TODO - add your code to create a functional React component that renders account details for a logged in user. Fetch the account data from the provided API. You may consider conditionally rendering a message for other users that prompts them to log in or create an account.  */
 
-import React, { useEffect, useState } from 'react';
-
-import LoginForm from '../components/LoginForm';
-import { useUpdateBookAvailabilityMutation } from '../redux/api';
-
+import { useGetUsersQuery } from '../redux/api';
+import { Paper, Typography } from "@mui/material";
 
 
 
 
 
 export default function Account() {
-   const [loginData, setLoginData] = useState(null);
-   const [loading, setLoading] = useState(true);
-   const [error, setError] = useState(null);
-   const [ update ] = useUpdateBookAvailabilityMutation();
+ 
+   // const [ update ] = useUpdateBookAvailabilityMutation();
+   const { data: userDetails, isError, isLoading } = useGetUsersQuery()
     
+   // const handleLogout = () => {
+    // Perform logout action, e.g., clearing authentication state
+    // Redirect to the login page
+    // navigate("/login"); 
+  //  };
 
-  
 
-return (
-    <div>
-        {loading ? (
-            <p>Loading...</p>
-        ) : error ? (
-            <p>error</p>
-        ) : loginData ? (
-            <LoginForm loginData={loginData} />
-        ) : (
-            <p>Please log in to access your account details.</p>
-        ) }
-    </div>
-     
-);
-
-}
+   return (
+    <Paper elevation={6} sx={{ width: "50%", padding: 4, margin: "14px auto" }}>
+      {isLoading ? (
+        <Typography variant="body1">Loading user details...</Typography>
+      ) : isError ? (
+        <Typography variant="body1">Error fetching user details</Typography>
+      ) : userDetails ? (
+        <div>
+          <Typography variant="h5" sx={{ textAlign: "center" }}>
+            Account Details
+          </Typography>
+          <Typography variant="body1">
+            Welcome, {userDetails.firstname} {userDetails.lastname}!
+          </Typography>
+          <Typography variant="body1">Email: {userDetails.email}</Typography>
+          
+        </div>
+      ) : null}
+    </Paper>
+  );
+};
