7d1a42009cec109236de43df99d196b37bf2ca4f
meganm672 <meganm672@gmail.com>
2023-10-08 22:12:46 -0400

    tried to add the logout button in the nav bar but it is not working yet

--- src/App.jsx
+++ src/App.jsx
@@ -10,7 +10,7 @@ import SingleBook from './components/SingleBook'
 
 import { setToken } from './redux/tokenSlice'
 // import components from react router dom
-import { Routes, Route } from "react-router-dom"
+import { Routes, Route, useNavigate } from "react-router-dom"
 
 import { Button } from '@mui/material'
 
@@ -19,32 +19,25 @@ function App() {
   const token = useSelector(state => state.token);
   console.log("The Token is :", token)
 
-  const dispatch= useDispatch();
- 
-  
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
+
   return (
     <>
-      <h1><img id='logo-image' src={bookLogo}/>Library App</h1>
-
-      <p>Complete the React components needed to allow users to browse a library catalog, check out books, review their account, and return books that they've finished reading.</p>
-
-      <p>You may need to use the `token` in this top-level component in other components that need to know if a user has logged in or not.</p>
-
-      <p>Don't forget to set up React Router to navigate between the different views of your single page application!</p>
-    <Navigations />
-    {token && (
-      <Button onClick={() => dispatch(setToken({ token: null }))} >Logout</Button>
-    )}
-    {/* {token ? <Account /> : <LoginForm />} */}
+      <Navigations />
+      {token && (
+        <Button onClick={() => {
+          dispatch(setToken({ token: null }))
+          navigate('/books')
+        }}
+        >Logout</Button>
+      )}
       <Routes>
-        <Route path="/books" element={<Books />}/>
+        <Route path="/books" element={<Books />} />
         <Route path="/books/:id" element={<SingleBook />} />
         <Route path="/login" element={<LoginForm />} />
-        {/* <Route path="/register" element={<Register/>} /> */}
-        {token && <Route path="/account" element={<Account />}/>}
+        {token && <Route path="/account" element={<Account />} />}
       </Routes>
-
-
     </>
   )
 }
--- src/components/Navigations.jsx
+++ src/components/Navigations.jsx
@@ -4,10 +4,10 @@ for example 'Login' should be available if someone has not logged in yet. */
 import React from 'react';
 
 //import Link as RouterLink for mui 
-import { Link as RouterLink} from 'react-router-dom'
+import { Link as RouterLink, useNavigate } from 'react-router-dom'
 //import the booklogo
 import bookLogo from '../assets/books.png'
-import { useSelector } from 'react-redux'
+import { useSelector, useDispatch } from 'react-redux'
 //import the mui components
 import Button from '@mui/material/Button';
 import Menu from '@mui/material/Menu';
@@ -20,21 +20,24 @@ import IconButton from '@mui/material/IconButton';
 import MenuIcon from '@mui/icons-material/Menu';
 
 // import use slectro to get token and do token && login and account 
-const Navigations= () =>{
+const Navigations = () => {
   const token = useSelector(state => state.token);
-    const [anchorEl, setAnchorEl] = React.useState(null);
-    const open = Boolean(anchorEl);
+  const [anchorEl, setAnchorEl] = React.useState(null);
+  const open = Boolean(anchorEl);
 
-    const handleClick = (event) => {
-      setAnchorEl(event.currentTarget);
-    };
+  const dispatch = useDispatch();
+  const navigate = useNavigate();
 
-    const handleClose = () => {
-      setAnchorEl(null);
-    };
+  const handleClick = (event) => {
+    setAnchorEl(event.currentTarget);
+  };
 
-    return(
-     <Box sx={{ flexGrow: 1 }}>
+  const handleClose = () => {
+    setAnchorEl(null);
+  };
+
+  return (
+    <Box sx={{ flexGrow: 1 }}>
       <AppBar position="static">
         <Toolbar>
           <IconButton
@@ -49,35 +52,47 @@ const Navigations= () =>{
             aria-expanded={open ? 'true' : undefined}
             onClick={handleClick}
           >
-            <MenuIcon  />
+            <MenuIcon />
           </IconButton>
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
-            <img id='logo-image' src={bookLogo}/> Library App
+            <img id='logo-image' src={bookLogo} /> Library App
           </Typography>
-          
-          {!token && <Button color="inherit" component={RouterLink} to="/login">Login</Button>}
+          {/* {!token ? (<Button color="inherit" component={RouterLink} to="/login">Login</Button>)
+            : (<Button onClick={() => {
+              dispatch(setToken({ token: null }))
+              navigate('/books')
+            }}
+            >Logout</Button>)} */}
+           {/* {token && ( 
+             <Button onClick={() => { 
+            dispatch(setToken({ token: null }))
+              navigate('/books')
+            }}
+             >Logout</Button>
+           )} */}
+           {!token && <Button color="inherit" component={RouterLink} to="/login">Login</Button>}
           <Menu
-        id="demo-positioned-menu"
-        aria-labelledby="demo-positioned-button"
-        anchorEl={anchorEl}
-        open={open}
-        onClose={handleClose}
-        anchorOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-        }}
-        transformOrigin={{
-            vertical: 'top',
-            horizontal: 'left',
-        }}
-      >
-        <MenuItem onClick={handleClose} component={RouterLink} to="/books">Home</MenuItem>
-        {token && <MenuItem onClick={handleClose} component={RouterLink} to="/account">Account</MenuItem>}
-      </Menu>
+            id="demo-positioned-menu"
+            aria-labelledby="demo-positioned-button"
+            anchorEl={anchorEl}
+            open={open}
+            onClose={handleClose}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'left',
+            }}
+          >
+            <MenuItem onClick={handleClose} component={RouterLink} to="/books">Home</MenuItem>
+            {token && <MenuItem onClick={handleClose} component={RouterLink} to="/account">Account</MenuItem>}
+          </Menu>
         </Toolbar>
       </AppBar>
     </Box>
-    )
+  )
 }
 
 export default Navigations
